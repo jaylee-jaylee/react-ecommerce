52eb7d6cf8b949cd2892fa0ffc1e3eb34b19d151
EC2 Default User <ec2-user@ip-172-31-33-191.ap-northeast-1.compute.internal>
2019-11-14 04:49:56 +0000

    fix typo

--- src/config/config.js
+++ src/config/config.js
@@ -1,7 +1,7 @@
 const API_SERVER = 'http://54.180.134.177:5000'
 
 const CONFIG = {
-  EVENT_REGISTER: `${API_SERVER}/evetns/register`,
+  EVENT_REGISTER: `${API_SERVER}/events/register`,
   USER_LOGIN: `${API_SERVER}/users/authenticate`,
   USER_SIGNUP: `${API_SERVER}/users/register`,
   COLLECTION: `${API_SERVER}/collection`
